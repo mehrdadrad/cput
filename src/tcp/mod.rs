c8621b2afcac5d7e5379a10e7b17bb47b1ae04e7
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2020-04-13 20:59:59 -0700

    add basic tokio tcp client server

--- src/tcp/mod.rs
+++ src/tcp/mod.rs
@@ -1,12 +1,65 @@
-pub struct Server<'a> {
-    pub addr: &'a str,
-}
+#![allow(dead_code)]
 
-impl<'a> Server<'a> {
-    fn new() -> Server<'a> {
-        Server {
-            addr: "0.0.0.0:3057",
+use std::time::Duration;
+use tokio::net::TcpListener;
+use tokio::net::TcpStream;
+use tokio::prelude::*;
+use tokio::task;
+
+pub struct Server {}
+pub struct Client {}
+
+impl Server {
+    pub fn new() -> Server {
+        Server {}
+    }
+
+    #[tokio::main]
+    pub async fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let mut listener = TcpListener::bind("127.0.0.1:3057").await?;
+        loop {
+            let (mut socket, _) = listener.accept().await?;
+            tokio::spawn(async move {
+                let mut buf = [0; 1024];
+                loop {
+                    let n = match socket.read(&mut buf).await {
+                        Ok(n) if n == 0 => return,
+                        Ok(n) => n,
+                        Err(e) => {
+                            eprintln!("failed to read from socket; err = {:?}", e);
+                            return;
+                        }
+                    };
+
+                    if let Err(e) = socket.write_all(&buf[0..n]).await {
+                        eprintln!("failed to write to socket; err = {:?}", e);
+                        return;
+                    }
+                }
+            });
         }
     }
-    fn run() {}
+}
+
+impl Client {
+    pub fn new() -> Client {
+        Client {}
+    }
+
+    #[tokio::main]
+    pub async fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
+        loop {
+            let mut stream = TcpStream::connect("127.0.0.1:3057").await?;
+            tokio::spawn(async move {
+                match stream.write(b"cput cput cput").await {
+                    Ok(_) => println!("write"),
+                    Err(e) => eprintln!("{}", e),
+                }
+
+                drop(stream);
+            });
+
+            task::spawn_blocking(|| std::thread::sleep(Duration::from_millis(50))).await?;
+        }
+    }
 }
