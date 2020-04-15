23a7bbd2fab514b2ad027651eb8bcd105b70aa9d
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2020-04-15 16:42:24 -0700

    optimized

--- src/tcp/mod.rs
+++ src/tcp/mod.rs
@@ -1,65 +1,50 @@
-#![allow(dead_code)]
-
+use std::io::Error;
 use std::time::Duration;
+use tokio::io::AsyncWriteExt;
 use tokio::net::TcpListener;
 use tokio::net::TcpStream;
 use tokio::prelude::*;
-use tokio::task;
+use tokio::time;
 
-pub struct Server {}
-pub struct Client {}
-
-impl Server {
-    pub fn new() -> Server {
-        Server {}
-    }
-
-    #[tokio::main]
-    pub async fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
-        let mut listener = TcpListener::bind("127.0.0.1:3057").await?;
-        loop {
-            let (mut socket, _) = listener.accept().await?;
-            tokio::spawn(async move {
-                let mut buf = [0; 1024];
-                loop {
-                    let n = match socket.read(&mut buf).await {
-                        Ok(n) if n == 0 => return,
-                        Ok(n) => n,
-                        Err(e) => {
-                            eprintln!("failed to read from socket; err = {:?}", e);
-                            return;
-                        }
-                    };
-
-                    if let Err(e) = socket.write_all(&buf[0..n]).await {
-                        eprintln!("failed to write to socket; err = {:?}", e);
+pub async fn server() -> Result<(), Error> {
+    let mut listener = TcpListener::bind("127.0.0.1:3057").await?;
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    Ok(n) if n == 0 => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("failed to read from socket; err = {:?}", e);
                         return;
                     }
+                };
+
+                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                    eprintln!("tcp client write error: {} ", e);
+                    return;
                 }
-            });
-        }
+            }
+        });
     }
 }
 
-impl Client {
-    pub fn new() -> Client {
-        Client {}
-    }
+pub async fn client() -> Result<(), Error> {
+    let mut stream = TcpStream::connect("127.0.0.1:3057").await?;
+    let mut interval = time::interval(Duration::from_millis(10));
 
-    #[tokio::main]
-    pub async fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
-        loop {
-            let mut stream = TcpStream::connect("127.0.0.1:3057").await?;
-            tokio::spawn(async move {
-                match stream.write(b"cput cput cput").await {
-                    Ok(_) => println!("write"),
-                    Err(e) => eprintln!("{}", e),
-                }
+    let msg = [0_u8; 1024];
 
-                drop(stream);
-            });
-
-            task::spawn_blocking(|| std::thread::sleep(Duration::from_millis(50))).await?;
+    loop {
+        match stream.write(&msg).await {
+            Ok(_) => {}
+            Err(e) => {
+                eprintln!("client error: {}", e);
+                stream = TcpStream::connect("127.0.0.1:3057").await?;
+            }
         }
+        interval.tick().await;
     }
 }
